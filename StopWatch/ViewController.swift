9c8eeb38613f4ec5e1dd434e959c0010a16124ad
Vigneshraj Sekar Babu <vigneshrajsekarbabu@vigneshrajs-macbook-pro.local>
2018-05-13 23:45:28 -0500

    trying a stop watch and a timer app

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -7,19 +7,30 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var timeLabel: UILabel!
+  //  let startTime : NSDate = NSDate()
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func startPressed(_ sender: UIButton) {
+       
     }
-
-
+    
+    @IBAction func endDate(_ sender: UIButton) {
+    }
+    
+    @IBAction func lapButtonPressed(_ sender: UIButton) {
+    }
+    
 }
 
